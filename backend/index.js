308b8945ddfa38dba3e977b07c937dff10f4e8dd
Marcio Marques Rosa <marcio.rosa@logusinfo.com.br>
2020-03-25 16:41:23 -0300

    Criando a aplicação e fazendo com que escute na porta 3333

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,5 @@
 const express = require('express'); //Importando o módulo express.
+
+const app = express(); //Criando a aplicação.
+
+app.listen(3333); //Fazendo a aplicação escutar a porta 3333 (localhost:3333).
