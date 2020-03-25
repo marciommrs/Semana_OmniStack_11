327c49211a47adfd0aae0786e86781157d5b8b5c
Marcio Marques Rosa <marcio.rosa@logusinfo.com.br>
2020-03-25 17:09:00 -0300

    Criando uma rota padrão e uma resposta.

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,8 @@ const express = require('express'); //Importando o módulo express.
 
 const app = express(); //Criando a aplicação.
 
+app.get('/', (request, response) => {
+  return response.send('Hello world!');
+}); //Criando a rota raiz.
+
 app.listen(3333); //Fazendo a aplicação escutar a porta 3333 (localhost:3333).
