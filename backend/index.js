96035d0c55965bd62f311698a83c41f5a728fb5e
Marcio Marques Rosa <marcio.rosa@logusinfo.com.br>
2020-03-25 17:13:58 -0300

    Retornando uma objeto Json na rota padrão.

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const express = require('express'); //Importando o módulo express.
 const app = express(); //Criando a aplicação.
 
 app.get('/', (request, response) => {
-  return response.send('Hello world!');
-}); //Criando a rota raiz.
+  return response.json({
+    evento:'Semana OmniStack 11.0',
+    aluno: 'Marcio Marques Rosa'
+  });
+}); //Criando a rota raiz retornando um objeto Json.
 
 app.listen(3333); //Fazendo a aplicação escutar a porta 3333 (localhost:3333).
