f6d8d5f6fb453b7e95ddfd7262c0198f4c1f95af
Marcio Marques Rosa <marcio.rosa@logusinfo.com.br>
2020-03-26 08:04:56 -0300

    Configuração do arquivo migrate para criação da tabela de ongs

--- backend/src/database/migrations/20200326074614_create_ongs.js
+++ backend/src/database/migrations/20200326074614_create_ongs.js
@@ -1,8 +1,15 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex) { // Executado na criação da tabela.
+  return knex.schema.createTable('ongs', function (table) {
+    table.string('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('whatsapp').notNullable();
+    table.string('city').notNullable();
+    table.string('uf', 2).notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex) { // O que deve ser executado quando der problema.
+  return knex.schema.dropTable('ongs');
 };
