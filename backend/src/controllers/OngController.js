7edd0e7fbf3b66433ffc3d08a7d4f8ed46861ba1
Marcio Marques Rosa <marcio.rosa@logusinfo.com.br>
2020-04-01 19:46:56 -0300

    Organizando acesso a tabela de ongs no OngController

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,33 @@
+const connection = require('../database/connection'); //Importando a conexão.
+const crypto = require('crypto'); //Importando pacote de criptografia do note.
+
+module.exports = {
+  //------------------------------------------------------------------------------
+  // List ongs.
+  //------------------------------------------------------------------------------
+  async list(request, response) {
+    const ongs = await connection('ongs').select('*');
+
+    return response.json(ongs);
+  },
+
+  //------------------------------------------------------------------------------
+  // Create ongs.
+  //------------------------------------------------------------------------------
+  async create(request, response) {
+    const { name, email, whatsapp, city, uf } = request.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    });
+
+    return response.json({ id });
+  }
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express'); //Importando o módulo express.
-const crypto = require('crypto'); //Importando pacote de criptografia do note.
-const connection = require('./database/connection'); //Importando a conexão.
+const OngController = require('./controllers/OngController')
+
 
 const routes = express.Router(); //Desacoplar o módulo de rotas na variável routes.
 
@@ -17,38 +17,19 @@ routes.get('/', (request, response) => {
 
 
 //------------------------------------------------------------------------------
-// Create ongs route.
+// List ongs route.
 //------------------------------------------------------------------------------
 routes.get('/ongs', async (request, response) => {
-  const ongs = await connection('ongs').select('*');
-
-  return response.json(ongs);
+  return OngController.list(request, response)
 })
-
 
 
 //------------------------------------------------------------------------------
 // Create ongs route.
 //------------------------------------------------------------------------------
 routes.post('/ongs', async (request, response) => {
-  createOng(request, response)
+  return OngController.create(request, response)
 });
 
-async function createOng(request, response) {
-  const {name, email, whatsapp, city, uf} = request.body;
-
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf
-  });
-
-  return response.json({ id });
-}
 
 module.exports = routes; //Exportar a variável routes como módulo.
