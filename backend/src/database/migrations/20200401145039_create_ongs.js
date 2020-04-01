7ff64cd369f38d62e73b8e6368856d59fdeaa567
Marcio Marques Rosa <marcio.rosa@logusinfo.com.br>
2020-04-01 14:56:14 -0300

    Ajustes nas tabelas de ongs e incidents

--- backend/src/database/migrations/20200401145039_create_ongs.js
+++ backend/src/database/migrations/20200401145039_create_ongs.js
@@ -1,7 +1,8 @@
 
-exports.up = function(knex) { // Executado na criação da tabela.
+exports.up = function(knex) {
   return knex.schema.createTable('ongs', function (table) {
     table.string('id').primary();
+    
     table.string('name').notNullable();
     table.string('email').notNullable();
     table.string('whatsapp').notNullable();
@@ -10,6 +11,6 @@ exports.up = function(knex) { // Executado na criação da tabela.
   });
 };
 
-exports.down = function(knex) { // O que deve ser executado quando der problema.
+exports.down = function(knex) {
   return knex.schema.dropTable('ongs');
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,41 @@
 const express = require('express'); //Importando o módulo express.
+const crypto = require('crypto'); //Importando pacote de criptografia do note.
+const connection = require('./database/connection'); //Importando a conexão.
 
 const routes = express.Router(); //Desacoplar o módulo de rotas na variável routes.
 
+
+//------------------------------------------------------------------------------
+// Default route.
+//------------------------------------------------------------------------------
 routes.get('/', (request, response) => {
   return response.json({
     evento:'Semana OmniStack 11.0',
     aluno: 'Marcio Marques Rosa'
   });
 }); //Criando a rota raiz retornando um objeto Json.
+
+
+
+//------------------------------------------------------------------------------
+// Create ongs route.
+//------------------------------------------------------------------------------
+routes.post('/ongs', async (request, response) => {
+  createOng(request, response)
+});
+
+async function createOng(request, response) {
+  const {name, email, whatsapp, city, uf} = request.body;
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id, name, email, whatsapp, city, uf
+  });
+
+  console.log(data);
+
+  return response.json(data);
+}
 
 module.exports = routes; //Exportar a variável routes como módulo.
