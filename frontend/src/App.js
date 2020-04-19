213453b88e9858bc16aef5af77ae300a98ee02cc
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-04-19 09:18:56 -0300

    Criação da página de logon.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,12 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+
+import './global.css'
+
+import Logon from './pages/Logon'; // Sempre que se importa uma página, é procurado o arquivo index
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Logon />
   );
 }
 
