f17974736577852411453a190c554b9a56bf4bdd
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-04-20 11:10:54 -0300

    Adicionando funcão de Logout.

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link }  from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -10,13 +10,10 @@ import logoImg from '../../assets/logo.svg';
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
 
+  const history = useHistory();
+
   const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
-
-  async function loadIncidents() {
-    const response = await api.get('/profile', {headers: {Authorization: ongId}});
-    setIncidents(response.data);
-  }
   
   /* Parâmetros:
   1 - Função a ser carregada.
@@ -24,6 +21,14 @@ export default function Profile() {
   2.1 - Sem que algo dentro do array alterar, a função será executada.
   2.2 - Array vazio indica que a função será invocada uma única vez. */
   useEffect(loadIncidents, [ongId]);
+  
+  function loadIncidents() {
+    api
+      .get('/profile', { headers: { Authorization: ongId } })
+      .then((response) => {
+        setIncidents(response.data);
+      });
+  }
   
   async function handleDeleteIncident(id) {
     try {
@@ -34,15 +39,10 @@ export default function Profile() {
     }
   }
 
-  /*useEffect(() => {
-    api.get('/profile', {
-      headers: {
-        Authorization: ongId
-      }
-    }).then(response => {
-      setIncidents(response.data);
-    })
-  }, [ongId]); */
+  function handleLogout() {
+    localStorage.clear();
+    history.push('/');
+  }
 
   return (
     <div className="profile-container">
@@ -51,7 +51,8 @@ export default function Profile() {
         <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-        <button type="button">
+        
+        <button onClick={handleLogout} type="button">
           <FiPower size={18} color="#E02041" />
         </button>
       </header>
