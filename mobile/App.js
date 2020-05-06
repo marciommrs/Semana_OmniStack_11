6602aad83ea48460836c47354063a470a14e1e0c
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-05-06 05:41:03 -0300

    Configurações iniciais de ícone e splash screen.

--- mobile/App.js
+++ mobile/App.js
@@ -4,16 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.title}>Hello Omnistack</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, //Ocupar o tamanho todo da tela
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
