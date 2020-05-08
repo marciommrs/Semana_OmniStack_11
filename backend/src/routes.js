7acf2cc3843673aa860c1135f948cb58d4f82cc6
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-05-08 07:43:23 -0300

    Adicionando validação nas rotas com celebrate

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,10 @@
 const express = require('express'); //Importando o módulo express.
-const OngController = require('./controllers/OngController')
-const IncidentController = require('./controllers/IncidentController')
-const ProfileController = require('./controllers/ProfileController')
-const SessionController = require('./controllers/SessionController')
+const { celebrate, Segments, Joi } = require('celebrate'); //Importando módulo de validação
 
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router(); //Desacoplar o módulo de rotas na variável routes.
 
@@ -19,22 +20,71 @@ routes.get('/', (request, response) => {
 }); //Criando a rota raiz retornando um objeto Json.
 
 
-routes.post('/sessions', SessionController.create);
+// Login
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required()
+  })
+}), SessionController.create);
+
 
 //------------------------------------------------------------------------------
 // Ongs routes.
 //------------------------------------------------------------------------------
-routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
 
-routes.get('/profile', ProfileController.index);
+// Listar ONGs
+routes.get('/ongs', OngController.index);
+
+// Criar ONG
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}),OngController.create);
+
+// Listar incidentes por ONG
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown()
+}),ProfileController.index);
 
 //------------------------------------------------------------------------------
 // Incidents route.
 //------------------------------------------------------------------------------
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+
+// Listar incidentes
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}),IncidentController.index);
+
+// Criar incidentes
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown(),
+  [Segments.BODY] : Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required()
+  })
+}),IncidentController.create);
+
+// Apagar incidente
+routes.delete('/incidents/:id', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown(),
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}),IncidentController.delete);
 
 
 module.exports = routes; //Exportar a variável routes como módulo.
